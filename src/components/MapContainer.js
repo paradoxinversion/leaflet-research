db396cc9c859e57a6c8fbab121bcf382152b4683
Jedai Saboteur <paradoxinversion@gmail.com>
2018-09-13 17:59:43 -0700

    Refactored to use react-leaflet

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,58 +1,63 @@
-import React, { Component } from "react";
+import React, { createRef, Component } from "react";
 import "leaflet/dist/leaflet.css";
 import * as L from "leaflet";
+import { Map as LeafletMap, Popup, TileLayer } from "react-leaflet";
+import { render } from "react-dom";
 export default class MapContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      myMap: null,
-      popup: L.popup(),
-      initialCoordinations: [51.505, -0.09],
+      initialCoordinates: [51.505, -0.09],
       initialZoom: 13,
-      maxZoom: 18
+      maxZoom: 18,
+      latlng: null,
+      popupOpen: false
     };
-    this.openDefaultPopup = this.openDefaultPopup.bind(this);
-  }
-
-  openDefaultPopup(e) {
-    this.state.popup
-      .setLatLng(e.latlng)
-      .setContent("You clicked the map at: " + e.latlng)
-      .openOn(this.state.myMap);
+    this.mapRef = createRef();
   }
 
   onMapClicked(e) {
-    this.openDefaultPopup(e);
+    this.setState({
+      latlng: e.latlng,
+      popupOpen: true
+    });
   }
 
-  async componentDidMount() {
-    // ! accessToken needs to be pulled in a different way prior to deploy
-    const myMap = await L.map("my-map").setView(
-      this.state.initialCoordinations,
-      this.state.initialZoom
-    );
-    L.tileLayer(
-      "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
-      {
-        attribution:
-          'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: this.state.maxZoom,
-        id: "mapbox.streets",
-        accessToken: process.env.MAPBOX_ACCESS_TOKEN
-      }
-    ).addTo(myMap);
-
-    myMap.on("click", this.onMapClicked, this);
-    this.setState({
-      myMap
-    });
+  renderPopup() {
+    return this.state.popupOpen ? (
+      <Popup
+        position={this.state.latlng}
+        onClose={() => {
+          this.setState({
+            popupOpen: false
+          });
+        }}
+      >
+        <span>
+          Map clicked at {this.state.latlng.lat}, {this.state.latlng.lng}
+        </span>
+      </Popup>
+    ) : null;
   }
 
   render() {
     return (
-      <div>
-        <div id="my-map" />
-      </div>
+      <LeafletMap
+        id="my-map"
+        center={this.state.initialCoordinates}
+        zoom={this.state.initialZoom}
+        ref={this.mapRef}
+        onClick={this.onMapClicked.bind(this)}
+      >
+        <TileLayer
+          attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+          url="https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}"
+          id="mapbox.streets"
+          accessToken={process.env.MAPBOX_ACCESS_TOKEN}
+          maxZoom={this.state.maxZoom}
+        />
+        {this.renderPopup()}
+      </LeafletMap>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "leaflet/dist/leaflet.css";
 
 var mountNode = document.getElementById("app");
 ReactDOM.render(<App />, mountNode);
